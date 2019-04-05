f33020aedf63b05263ab99a972d81561a8d9180b
RequireSun <kelvinlpsun@hotmail.com>
2019-04-05 22:47:37 +0800

    feat: typescript 的声明引入进来了

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# clone-detection
+clone-detection
+======
+
 A clone detection tool for javascript.
+
+## 参考
+
+[一种改进的基于抽象语法树的软件源代码比对算法](http://netinfo-security.org/CN/article/downloadArticleFile.do?attachType=PDF&id=3520)
+
+[GitHub: AST explorer](https://github.com/fkling/astexplorer)
--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,18 +1,19 @@
 import { parseNoPatch } from 'babel-eslint';
+import { ParserOptions } from '@babel/parser';
+
+export const locationProps = new Set(['loc', 'start', 'end', 'range']);
+
+export function parse(code: string) {
+    const opts: ParserOptions = {
+        sourceType: 'module',
+    };
+
+    const ast = parseNoPatch(code, opts);
+    delete ast.tokens;
+    return ast;
+}
 
 export default {
-    locationProps: new Set(['loc', 'start', 'end', 'range']),
-
-    parse(code: string) {
-        const opts = {
-            sourceType: 'module',
-        };
-
-        const ast = parseNoPatch(code, opts);
-        delete ast.tokens;
-        return ast;
-    },
-
     nodeToRange(node: { [prop: string]: any }) {
         if (typeof node.start !== 'undefined') {
             return [node.start, node.end];
